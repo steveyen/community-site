b7db6973ccde894f1af5bbbd9b02c7f4cd22fadf
Steve Yen <steve.yen@gmail.com>
2008-10-27 11:19:39 -0700

    renamed recent_blog_posts helper to blog_posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 require 'haml'
 
 module ApplicationHelper
-  def recent_blog_posts(offset = 0, limit = 3)
+  def blog_posts(offset = 0, limit = 3)
     posts = IO.readlines("#{RAILS_ROOT}/app/views/blog/_posts.html.haml", "\.blog_post\n")
     haml  = ".blog_post\n" + posts[(offset + 1)..(offset + limit)].join
     Haml::Engine.new(haml).render
