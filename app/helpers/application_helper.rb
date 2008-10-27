14aa086f3dfc485a12322345636e056be44e1ad6
Steve Yen <steve.yen@gmail.com>
2008-10-27 11:15:29 -0700

    recent_blog_posts helper to get just a subset of blog posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
-# Methods added to this helper will be available to all templates in the application.
+require 'haml'
+
 module ApplicationHelper
+  def recent_blog_posts(offset = 0, limit = 3)
+    posts = IO.readlines("#{RAILS_ROOT}/app/views/blog/_posts.html.haml", "\.blog_post\n")
+    haml  = ".blog_post\n" + posts[(offset + 1)..(offset + limit)].join
+    Haml::Engine.new(haml).render
+  end
 end
