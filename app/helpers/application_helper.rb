1f0ba2067826b8f5d0a1f4759f47c1f81480e1ae
Steve Yen <steve.yen@gmail.com>
2008-10-29 11:21:38 -0700

    removed hardcoded memcachedsource.org from haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,18 @@
 require 'haml'
 
 module ApplicationHelper
+  def site_domain_name
+    ActiveRecord::Base.configurations[RAILS_ENV]['site_domain_name']
+  end
+
+  def support_email_address
+    'support@' + site_domain_name
+  end
+
   def blog_posts(offset = 0, limit = 3)
     posts = IO.readlines("#{RAILS_ROOT}/app/views/blog/_posts.html.haml", "\.blog_post\n")
     haml  = ".blog_post\n" + posts[(offset + 1)..(offset + limit)].join
     Haml::Engine.new(haml).render
   end
 end
+
